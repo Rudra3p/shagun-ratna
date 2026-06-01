f526f0c223b3f8bee270468a9114b9bf31f371e6
rudra <rudra090207@gmail.com>
2026-06-01 22:30:49 +0530

    update60

--- app/page.jsx
+++ app/page.jsx
@@ -1,8 +1,3 @@
-"use client";
-
-import React from 'react';
-import { motion } from 'framer-motion';
-import Image from 'next/image';
 import HeroSection from '@/sections/home/hero';
 
 <HeroSection />
