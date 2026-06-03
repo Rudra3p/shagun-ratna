94711fa98c01fe08fd612bb6a125ff6c0b52bc0f
rudra <rudra090207@gmail.com>
2026-06-03 20:51:38 +0530

    update83.2

--- components/PageDivider.jsx
+++ components/PageDivider.jsx
@@ -8,7 +8,7 @@ export default function PageDivider() {
     <div className="w-full py-16 px-6 flex justify-center">
       <motion.div 
         initial={{ width: "20%", opacity: 0 }}
-        whileInView={{ width: "60%", opacity: 1 }} // Increased width here
+        whileInView={{ width: "80%", opacity: 1 }} // Increased width here
         viewport={{ once: true }}
         transition={{ duration: 1.5, ease: "easeInOut" }}
         className="h-[1px] bg-gradient-to-r from-transparent via-[#C5A059] to-transparent"
