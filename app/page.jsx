c972028648d196c15d6dbdfd52059d6dfd21caf3
rudra <rudra090207@gmail.com>
2026-06-06 16:57:15 +0530

    update86.8

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,6 @@
 "use client";
 
-import HeroSection from '@/components/HeroSection';
+import HeroSection from '@/sections/home/HeroSection';
 import PageDivider from '@/components/PageDivider';
 import StorySection from '@/sections/home/Story';
 import FeaturedCollections from '@/sections/home/FeaturedCollections';
