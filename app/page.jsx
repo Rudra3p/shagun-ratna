7e6ecd16ea7cbb93b95af9d02bd9468f1c7c9306
rudra <rudra090207@gmail.com>
2026-06-06 16:49:20 +0530

    update86.4

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,6 @@
 import HeroSection from '@/sections/home/Hero';
-import PageDivider from '@/sections/home/Story.jsx';
+import PageDivider from '@/sections/home/PageDivider';
+import StorySection from '@/sections/home/StorySection';
 import FeaturedCollections from '@/sections/home/FeaturedCollections';
 import ProductGrid from '@/sections/home/ProductGrid';
 
