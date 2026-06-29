ed7148feb070e919ad8f1f136f5e61937b7f7587
rudra <rudra090207@gmail.com>
2026-06-30 01:51:08 +0530

    update93.6.4

---
+++ app/api/admin/dashboard/route.ts
@@ -0,0 +1,6 @@
+// import { getDashboardStats } from "@/controllers/adminDashController"; // Ensure your path is correct
+
+export async function GET() {
+  return console.log("Dashboard stats endpoint hit");
+  //  await getDashboardStats();
+}
